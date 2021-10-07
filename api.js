4bba1e25e8ed755a102570fd6abfaf6cd7f703c9
Anurag Saraswat <anu01saraswat@gmail.com>
2021-10-07 15:32:59 +0530

    LastFM api

---
+++ api.js
@@ -0,0 +1,17 @@
+
+const root_url = "http://ws.audioscrobbler.com/2.0"
+const api_key = "4a16f1c32796c466a1347458ab1626f4"
+async function getData(track, artist) {
+    track = track.replace(/ /g, "_")
+    artist = artist.replace(/ /g, "_")
+    const url = `${root_url}/?method=track.getInfo&api_key=${api_key}&artist=${artist}&track=${track}&format=json`
+    console.log(url)
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    data = await response.json()
+    console.log(data)
+}
