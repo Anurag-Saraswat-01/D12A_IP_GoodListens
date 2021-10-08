9108955bf9eeb46483ca3f0b5f2c2b88ae674c06
Varun007-program <varuntripathy04@gmail.com>
2021-10-08 21:13:09 +0530

    home added

--- good-listens/src/App.js
+++ good-listens/src/App.js
@@ -1,67 +1,51 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
-// import logo from './images/logo.png';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import logo from './images/logo.png';
+import bgimage from './images/music2.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="container">
-      {/* <Navbar bg="dark" variant="light">
+    <div className="container-fluid" style={{ paddingLeft: 0, paddingRight: 0 }}>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="#home" style = {{ fontSize: "36px"}}>
             <img
               alt=""
               src={logo}
-              width="150"
-              height="150"
-              className="d-inline-block align-top"
+              width="120"
+              height="120"
+              className="d-inline-block align-center"
             />{' '}
             GoodListens
 
-            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-      {/* <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto my-2 my-lg-0">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-
-              </Nav>
-            </Navbar.Collapse>
           </Navbar.Brand>
 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
+            <Nav style = {{ gap: "25px"}} className="justify-content-end" >
+              <Nav.Link href="#home" style={{ fontSize: "25px" }}>Home</Nav.Link>
+              <Nav.Link href="#link" style={{ fontSize: "25px" }}>About</Nav.Link>
+              <Form className="d-flex" style = {{ height: "40px", marginTop: "10px"}}>
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-light">Search</Button>
+              </Form>
 
-        </Container> */}
-
-
-      {/* </Navbar> */}
-
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
+      <div className="bg-image">
+        <img src={bgimage} alt="" width="100%" height="auto" />
+      </div>
 
-
-    </div >
+    </div>
   );
 }
 
